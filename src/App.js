37727f87c35fddbc5b529dcd47558c3f213a0d41
Dominik Zsiros <zsdominik@outlook.com>
2020-06-15 13:17:06 +0200

    Task1: Core concepts

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import "./App.css";
 import HelloWorld from "./HelloWorld/HelloWorld";
 
+// https://medium.com/better-programming/when-to-use-react-purecomponent-723f85738be1
+// class App extends PureComponent {
 class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <h1>Hello world</h1>
+        <h1>Hello world from jsx</h1>
         <HelloWorld />
       </div>
     );
